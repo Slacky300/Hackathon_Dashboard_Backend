d1d03db8b15725d94859f45575b3ddd6bb61a91d
Slacky300 <rehman.khan.300a@gmail.com>
2023-09-13 21:57:34 +0530

    bug fixes

--- controllers/userCntrl.js
+++ controllers/userCntrl.js
@@ -22,7 +22,7 @@ const getAllUsers = asyncHandler(async (req, res) => {
             "year": user.year
         })
     }
-    res.status(200).json(registered_members);
+    res.status(200).json(users);
 });
 
 
